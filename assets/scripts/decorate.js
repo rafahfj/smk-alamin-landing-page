ef45b846f7554806be037ca97ebd1b599b947f45
Rafah Fajri Juwaeni <rafahfajrijuwaeni@gmail.com>
2024-07-04 22:05:53 +0700

    Move IMG & Added Footer

--- assets/scripts/decorate.js
+++ assets/scripts/decorate.js
@@ -20,5 +20,34 @@ function addSeparate() {
                   <rect x="0" y="0" width="100%" height="100%" fill="url(#b-grad)"/>`;
   });
 }
-
 addSeparate();
+
+function showCards(m) {
+  return `<div class="col-md-4 my-5">
+  <div class="card">
+    <img src="${m.Poster}" class="card-img-top" alt="" />
+    <div class="card-body">
+      <h5 class="card-title">${m.Title}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">
+        ${m.Year}
+      </h6>
+      <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
+      data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+    </div>
+  </div>
+</div>`;
+}
+
+// function addFaq() {
+//   fetch("../database/faq.json")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((faqs) => {
+//       console.log(faqs);
+//       faqs.allFaqs.forEach((faq) => {
+//         console.log(faq);
+//       });
+//     });
+// }
+// addFaq();
